47281ceb4ab6c0d5362a7e644b54239c440290a5
Mariouz <joseph02252003@gmail.com>
2024-04-17 21:59:26 +0800

    connected to oracle db

--- main.java
+++ main.java
@@ -1,5 +1,28 @@
+import java.sql.*;
+
 class main{
     public static void main(String args[]) {
         System.out.println("Hello");
+
+        
+
+        String host = "jdbc:oracle:thin:@//db18.cse.cuhk.edu.hk:1521/oradb.cse.cuhk.edu.hk";
+        String username = "h017";
+        String password = "cicsEvva";
+        
+
+        try{
+            Connection con = DriverManager.getConnection(host, username, password);
+            System.out.println("Connected");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from SPONSORS");
+            while(rs.next()) {
+                System.out.println(rs.getInt("SID") + " " + rs.getString("SPONSOR_NAME") + " " + rs.getFloat("MARKET_VALUE"));
+            }
+            con.close();
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
