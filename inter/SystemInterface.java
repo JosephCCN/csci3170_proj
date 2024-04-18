0d0c0d172190bcc4dc9cf7ced47ef090e4edcfcb
Mariouz <joseph02252003@gmail.com>
2024-04-18 18:40:17 +0800

    can insert book data now

--- inter/SystemInterface.java
+++ inter/SystemInterface.java
@@ -2,6 +2,7 @@ package inter;
 
 import java.sql.*;
 import java.util.Scanner;
+import java.io.*;
 
 public class SystemInterface{
 
@@ -11,8 +12,9 @@ public class SystemInterface{
         stmt = s;
     }
 
-    public void printInterface() {
+    private void printInterface() {
         System.out.println("<This is the system interface.>");
+        System.out.println("-------------------------------");
         System.out.println("1. Create Table.");
         System.out.println("2. Delete Table.");
         System.out.println("3. Insert Data.");
@@ -22,7 +24,7 @@ public class SystemInterface{
     }
 
 
-    public void createTable() {
+    private void createTable() {
         String book = """
         CREATE TABLE book (
             ISBN char(13),
@@ -97,7 +99,7 @@ public class SystemInterface{
         }
     }
 
-    public void deleteTable() {
+    private void deleteTable() {
         String[] tables = {"have", "ordering", "Book_author", "customer", "orders", "author", "book"};
         try{
             for(int i=0;i<7;i++) {
@@ -110,11 +112,44 @@ public class SystemInterface{
         }
     }
 
-    public void insertData() {
+    private void insertBook(File file) {
+        try{
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parm = line.split("\\|", 0);
 
+                String isbn = parm[0];
+                String name = parm[1];
+                String price = parm[2];
+                String copies = parm[3];
+
+                String query = String.format("insert into book values ('%s', '%s', %s, %s)", isbn, name, price, copies);
+
+                // System.out.println(query);
+                this.stmt.executeQuery(query);
+            }
+            scanner.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+}
+
+    private void insertData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the folder path");
+        String path = scanner.nextLine();
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        for(int i=0;i<listOfFiles.length;i++) {
+            if(listOfFiles[i].getName().equals("book.txt")) {
+                insertBook(new File(path, "book.txt"));
+            }
+        }
     }
 
-    public void setSysDate() {
+    private void setSysDate() {
 
     }
 
